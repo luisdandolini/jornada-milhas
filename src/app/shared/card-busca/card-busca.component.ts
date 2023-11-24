c298d7ef2ec94061db4848085dbb18470f9fa98e
Luís Fernando Dandolini Duarte <lduarte@personalcard.com.br>
2023-11-24 15:28:11 -0300

    feat: get promocoes

--- src/app/shared/card-busca/card-busca.component.ts
+++ src/app/shared/card-busca/card-busca.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PromocaoService } from 'src/app/core/services/promocao.service';
+import { Promocao } from 'src/app/core/types/types';
 
 @Component({
   selector: 'app-card-busca',
   templateUrl: './card-busca.component.html',
   styleUrls: ['./card-busca.component.scss']
 })
-export class CardBuscaComponent {
+export class CardBuscaComponent implements OnInit {
+  promocoes: Promocao[] = []; 
+  
+  constructor(
+    private servicoPromocao: PromocaoService
+  ) { }
 
+  ngOnInit(): void {
+    this.servicoPromocao.listar()
+      .subscribe(
+        response => {
+          this.promocoes = response
+        }
+      )
+  }
 }
