9e1f146c0769694309e0476ab5d9a7062750a55b
Luís Fernando Dandolini Duarte <lduarte@personalcard.com.br>
2023-12-01 12:40:42 -0300

    feat: validators login

--- src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
+++ src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
@@ -29,7 +29,6 @@ export class DropdownUfComponent implements OnInit {
     this.unidadeFederativaService.listar()
       .subscribe(dados => {
         this.unidadesFederativas = dados
-        console.log(this.unidadesFederativas)
       })
     this.filteredOptions$ = this.control.valueChanges.pipe(
       startWith(''),
